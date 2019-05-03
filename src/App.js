9a2e0fda9c6d77309d7cf1c33d7fc975772844d9
syedad218 <syed@cheenilabs.com>
2019-05-03 11:51:55 +0530

    git-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Posts from './components/Posts';
+import { Segment } from 'semantic-ui-react'
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App-content">
+      <Segment raised style={{ padding: '1rem' }}>
+        <Posts/>        
+      </Segment>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Postform.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Form } from 'semantic-ui-react'
+
+
+class PostForm extends Component {
+  state = { name: '' }
+  handleSubmit = () => {
+    console.log('Not Implemented ... ')
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PostForm;
